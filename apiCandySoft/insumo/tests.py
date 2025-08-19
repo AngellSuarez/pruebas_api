82257c5bc84ce80fcc20e1a130b67e64c53e057a
angelsuaz <miguel.angel.cardona.suarez1@gmail.com>
2025-08-18 23:53:04 -0500

    pruebas unitarias insumo, marca, servicio

--- apiCandySoft/insumo/tests.py
+++ apiCandySoft/insumo/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
+from insumo.models import Marca, Insumo
 
 # Create your tests here.
+class InsumoModelTest(TestCase):
+    
+    def test_crear_marca(self):
+        nombre_marca = "marca prueba 23"
+        
+        marca = Marca.objects.create(nombre = nombre_marca)
+        
+        self.assertEqual(Marca.objects.count(), 1)
+        self.assertEqual(marca.nombre, nombre_marca)
+        self.assertEqual(str(marca), nombre_marca)
+        
+    def test_crear_insumo(self):
+        nombre_marca = "Marca de prueba"
+        marca = Marca.objects.create(nombre=nombre_marca)
+
+        datos_insumo = {
+            "nombre": "Lima de uñas",
+            "stock": 10,
+            "marca_id": marca
+        }
+        # se usa ** para pasar el diccionario como argumentos de palabra clave.
+        insumo = Insumo.objects.create(**datos_insumo)
+
+        self.assertEqual(Insumo.objects.count(), 1)
+        
+        # comprobar que el estado se haya actualizado a 'activo'
+        self.assertEqual(insumo.estado, "Activo")
+        
+    def test_insumo_estado_bajo(self):
+        marca = Marca.objects.create(nombre="Marca de prueba")
+        
+        # Crea un insumo con stock bajo
+        insumo = Insumo.objects.create(
+            nombre="Algodón",
+            stock=5,
+            marca_id=marca
+        )
+        
+        # comprobar que el estado se haya actualizado a 'Bajo'
+        self.assertEqual(insumo.estado, "Bajo")
+        
+    def test_insumo_estado_agotado(self):
+        marca = Marca.objects.create(nombre="Marca de prueba")
+        
+        insumo = Insumo.objects.create(
+            nombre="Gel para uñas",
+            stock=0,
+            marca_id=marca
+        )
+        
+        # comprobar que el estado se haya actualizado a 'Agotado'
+        self.assertEqual(insumo.estado, "Agotado")
--- apiCandySoft/servicio/tests.py
+++ apiCandySoft/servicio/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
-
+from servicio.models import Servicio
+from datetime import timedelta
+from django.core.exceptions import ValidationError
 # Create your tests here.
+class Test_servicio(TestCase):
+    
+    #setup de los datos 
+    def setUp(self):
+        
+        self.data_valida_servicio = {
+            'nombre':'Servicio prueba',
+            'descripcion':'soy una prueba pa probar',
+            'estado': 'Activo',
+            'tipo':'manicure',
+            'url_imagen':'http://soyunaurl.com/soyunaimagen.jpg',
+            'precio':20000,
+            'duracion': timedelta(minutes=70)
+        }
+        
+    def test_crear_servicio(self):
+        
+        servicio = Servicio.objects.create(**self.data_valida_servicio)
+        
+        self.assertEqual(Servicio.objects.count(),1)
+        self.assertEqual(servicio.nombre, 'Servicio prueba')
+        self.assertEqual(servicio.estado, 'Activo')
+        
+    def test_data_invalida_crear(self):
+        
+        data_invalida = self.data_valida_servicio.copy()
+        data_invalida['tipo'] = 'tontolon'
+        
+        with self.assertRaises(ValidationError):
+            servicio = Servicio(**data_invalida)
+            servicio.full_clean()
